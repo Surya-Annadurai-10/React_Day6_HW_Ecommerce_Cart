fcb3b6b6e1a45d6e3b90a6dde89539ed03266b04
Surya Annadurai <suryaannadurai1041999@gmail.com>
2024-12-29 14:24:41 +0530

    updated

--- src/Footer/Footer.jsx
+++ src/Footer/Footer.jsx
@@ -1,19 +1,26 @@
 import styles from './Footer.module.css'
 
-const Footer = () =>{
+const Footer = (props) =>{
+
+    const onClickClear = () =>{
+        props.setTotalPrice(0);
+        props.setClear(true);
+    }
     return (
         <>
-        <div className={styles.top}>
+      <div className={styles.footer}>
+      <div className={styles.top}>
             <div>
                 <h3 className={styles.Total}>Total</h3>
             </div>
             <div className={styles.total}>
-                <p className={styles.price}>$ 2199.99</p>
+                <p className={styles.price}>$ {props.totalPrice} </p>
             </div>
         </div>
         <div className={styles.button_box}>
-            <button className={styles.clear}>Clear Cart</button>
+            <button onClick={onClickClear} className={styles.clear}>Clear Cart</button>
         </div>
+      </div>
         </>
     )
 }
